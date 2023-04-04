8f5dbcd60c09786d84b5dea848acf1b555a08c5f
Artem Alimov <artemalimov333@gmail.com>
2023-04-04 19:35:12 +0300

    All

---
+++ Exercise 3/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace Exercise_3
+{
+    internal class Program
+    {
+        struct Price
+        {
+            public string NameItem { get; set; }
+            public string NameStore { get; set; }
+            public decimal PriceItem { get; set; }
+        }
+        static void Main(string[] args)
+        {
+            Price[] prices = new Price[2];
+            for (int i = 0; i < prices.Length; i++)
+            {
+                Console.WriteLine($"Input  {i + 1}  item:");
+                string nameItem = Console.ReadLine();
+                Console.Write("  Input Name Store: ");
+                string nameStore = Console.ReadLine();
+
+
+                decimal priceItem;
+                while (true)
+                {
+                    Console.Write("  Input Price: ");
+                    try
+                    {
+                        priceItem = decimal.Parse(Console.ReadLine());
+                        break;
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("Not decimal");
+                    }
+                }
+                prices[i] = new Price
+                {
+                    NameItem = nameItem,
+                    NameStore = nameStore,
+                    PriceItem = priceItem
+                };
+            }
+            prices = prices.OrderBy(x =>x.NameStore).ToArray();
+            Console.Write("Enter store name to display products: ");
+            string storeToDisplay = Console.ReadLine();
+
+            bool storeFound = false;
+
+            foreach (Price price in prices)
+            {
+                if (price.NameStore == storeToDisplay)
+                {
+                    storeFound = true;
+                    Console.WriteLine($"Product: {price.NameItem}, Price: {price.PriceItem}");
+                }
+            }
+
+            if (!storeFound)
+            {
+                throw new Exception("Store not found.");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Exercise 5/Program.cs
@@ -0,0 +1,82 @@
+﻿namespace Exercise_5
+{
+   
+
+  
+        class Calculator
+        {
+            public double Add(double num1, double num2)
+            {
+                return num1 + num2;
+            }
+
+            public double Sub(double num1, double num2)
+            {
+                return num1 - num2;
+            }
+
+            public double Mul(double num1, double num2)
+            {
+                return num1 * num2;
+            }
+
+            public double Div(double num1, double num2)
+            {
+                if (num2 == 0)
+                {
+                    throw new Exception("Cannot divide by zero.");
+                }
+
+                return num1 / num2;
+            }
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.Write("Enter the first number: ");
+                double num1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Enter the second number: ");
+                double num2 = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Enter the operation to perform (+, -, *, /): ");
+                char operation = Convert.ToChar(Console.ReadLine());
+
+                Calculator calculator = new Calculator();
+
+                double result = 0;
+
+                try
+                {
+                    switch (operation)
+                    {
+                        case '+':
+                            result = calculator.Add(num1, num2);
+                            break;
+                        case '-':
+                            result = calculator.Sub(num1, num2);
+                            break;
+                        case '*':
+                            result = calculator.Mul(num1, num2);
+                            break;
+                        case '/':
+                            result = calculator.Div(num1, num2);
+                            break;
+                        default:
+                            throw new Exception("Invalid operation.");
+                    }
+
+                    Console.WriteLine($"Result: {result}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error: {ex.Message}");
+                }
+
+                Console.ReadKey();
+            }
+        }
+    }
+
